32d6e53154254f12fbbeabacc2847a0ce3e01616
nastyaMakarenko <nastya.makarenko@ukr.net>
2015-12-12 11:59:34 +0200

    binarySearch

--- src/timeCounting/sortings/BubbleSorting.java
+++ src/timeCounting/sortings/BubbleSorting.java
@@ -21,7 +21,7 @@ public class BubbleSorting {
             }
         }
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] +  " ");
         }
 
     }
